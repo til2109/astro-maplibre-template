01ab00e5e3780361dab6f2c5a6e59bf3252dd866
mariogiampieri <mariogiampieri@gmail.com>
2024-11-14 20:49:28 -0500

    add raster interface

--- src/consts.ts
+++ src/consts.ts
@@ -1,30 +1,9 @@
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Project Website",
   EMAIL: "<your email address here>",
   DESCRIPTION: "<A description of your website>",
-};
-
-export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "<Portfolio for your work>",
-};
-
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
-};
-
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
-};
-
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects, with links to repositories and demos.",
 };
 
 export const SOCIALS: Socials = [
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+import type {
+  FillLayerSpecification,
+  CircleLayerSpecification,
+  LineLayerSpecification,
+  SymbolLayerSpecification,
+  RasterLayerSpecification,
+} from "maplibre-gl";
+
 export type Site = {
   NAME: string;
   EMAIL: string;
@@ -13,3 +21,54 @@ export type Socials = {
   NAME: string;
   HREF: string;
 }[];
+
+// add map interfaces here
+export interface GeoJSONFeatureLayer {
+  "data-type": "geojson";
+  id: string;
+  label?: string;
+  url: string;
+  toggle?: boolean;
+  visible?: boolean;
+  "layer-type":
+    | "symbol"
+    | "fill"
+    | "custom"
+    | "raster"
+    | "line"
+    | "circle"
+    | "heatmap"
+    | "fill-extrusion"
+    | "hillshade"
+    | "background";
+  paint?:
+    | FillLayerSpecification
+    | LineLayerSpecification
+    | CircleLayerSpecification
+    | SymbolLayerSpecification;
+  mouseEvent: MapEvent[];
+}
+
+export interface RasterLayer {
+  "data-type": "raster";
+  id: string;
+  label?: string;
+  url?: string;
+  tiles: string[];
+  bounds?: number[];
+  minzoom?: number;
+  maxzoom?: number;
+  tileSize?: number;
+  toggle?: boolean;
+  visible?: boolean;
+  "layer-type": "raster";
+  paint?: RasterLayerSpecification;
+  mouseEvent: MapEvent[];
+}
+
+export interface MapEvent {
+  type: "click" | "mousemove" | "mouseenter" | "mouseleave";
+  content: Array<{
+    [key: string]: string | ((e: maplibregl.MapLayerMouseEvent) => string);
+  }>;
+}
